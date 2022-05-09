40cdcb51761c2766d248ce8fe347e0dd1e4081bb
Grenier Célestin <cgrenier003@ensc.fr>
2022-05-09 08:52:12 +0200

    more cleaning

--- binding.gyp
+++ binding.gyp
@@ -1,7 +1,7 @@
 {
   "targets": [
     {
-      "target_name": "tree_sitter_test_binding",
+      "target_name": "tree_sitter_nasm_binding",
       "include_dirs": [
         "<!(node -e \"require('nan')\")",
         "src"
--- bindings/node/binding.cc
+++ bindings/node/binding.cc
@@ -4,7 +4,7 @@
 
 using namespace v8;
 
-extern "C" TSLanguage * tree_sitter_test();
+extern "C" TSLanguage * tree_sitter_nasm();
 
 namespace {
 
@@ -17,12 +17,12 @@ void Init(Local<Object> exports, Local<Object> module) {
 
   Local<Function> constructor = Nan::GetFunction(tpl).ToLocalChecked();
   Local<Object> instance = constructor->NewInstance(Nan::GetCurrentContext()).ToLocalChecked();
-  Nan::SetInternalFieldPointer(instance, 0, tree_sitter_test());
+  Nan::SetInternalFieldPointer(instance, 0, tree_sitter_nasm());
 
-  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("test").ToLocalChecked());
+  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("nasm").ToLocalChecked());
   Nan::Set(module, Nan::New("exports").ToLocalChecked(), instance);
 }
 
-NODE_MODULE(tree_sitter_test_binding, Init)
+NODE_MODULE(tree_sitter_nasm_binding, Init)
 
 }  // namespace
